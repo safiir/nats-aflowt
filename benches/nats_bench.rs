a3d586419b9b28651c52b7e761a6750943338d97
stevelr <steve@cosmonic.com>
2021-12-30 01:50:40 -0800

    use tokio runtime for benchmarks
    Signed-off-by: stevelr <steve@cosmonic.com>

--- benches/nats_bench.rs
+++ benches/nats_bench.rs
@@ -1,7 +1,8 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+
 use std::time::{Duration, Instant};
 
-pub async fn pub_benchmark(c: &mut Criterion) {
+pub fn pub_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("publish");
     group.warm_up_time(Duration::from_secs(1));
     // TODO(dlc) - Can both be done at some time or requires custom?
@@ -10,19 +11,18 @@ pub async fn pub_benchmark(c: &mut Criterion) {
 
     let bmsg: Vec<u8> = (0..32768).map(|_| 22).collect();
     for size in [32, 128, 256, 1024, 4096, 8192].iter() {
-        let nc = nats::connect("127.0.0.1").await.unwrap();
         let msg = &bmsg[0..*size];
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, _| {
-            b.iter_custom(|n| {
-                tokio::runtime::Handle::current().block_on(async {
+            b.to_async(tokio::runtime::Runtime::new().unwrap())
+                .iter_custom(|n| async move {
+                    let nc = nats::connect("127.0.0.1").await.unwrap();
                     let start = Instant::now();
                     for _i in 0..n {
                         nc.publish("bench", &msg).await.unwrap();
                     }
                     nc.flush().await.unwrap();
                     start.elapsed()
-                })
-            });
+                });
         });
     }
     group.finish();
