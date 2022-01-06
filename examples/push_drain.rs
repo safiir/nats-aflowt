4e2a74f3e350c2b87827a42b6444a3ba02aefedb
stevelr <steve@cosmonic.com>
2022-01-05 19:05:47 -0800

    two examples used to double-check doc tests
    Signed-off-by: stevelr <steve@cosmonic.com>

---
+++ examples/push_drain.rs
@@ -0,0 +1,20 @@
+// example taken from doc for PushSubscription::drain
+//
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sub_name = format!("drain_{}", rand::random::<u32>());
+    let client = nats::connect("demo.nats.io").await?;
+    let context = nats::jetstream::new(client);
+    context.add_stream(sub_name.as_str()).await?;
+    let mut subscription = context.subscribe("drain199").await?;
+    context.publish(&sub_name, "foo").await?;
+    context.publish(&sub_name, "bar").await?;
+    context.publish(&sub_name, "baz").await?;
+
+    subscription.drain().await?;
+
+    assert!(subscription.next().await.is_none());
+
+    println!("done");
+    Ok(())
+}
